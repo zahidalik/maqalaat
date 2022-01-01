9aadac59485ba1ec402e2b7d02d7ac5e344047ce
zahidalik <zahidkhan545@icloud.com>
2022-01-01 22:53:00 +0300

    add some pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def welcome
   end
+
+  def about
+    
+  end
+
+  def contact_us
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#welcome"
+  get "about", to: "home#about"
+  get "contact-us", to: "home#contact_us"
   resources :users
 end
